dea11b10a5c82308b4d2cb6f0b949ab39e111945
Sergio Abraham Martinez Rodriguez <sergio@engineer.blue>
2018-10-25 15:15:55 +0100

    [PlantExtraction] PCL Orientation
    Our IGL coordinate system is the inverse of the PCL one.
    We have to make them work nicely in the same coordinate system.

--- src/filters/plant_extraction.cpp
+++ src/filters/plant_extraction.cpp
@@ -32,6 +32,12 @@
 #include "../application_state.h"
 #include <igl/fit_plane.h>
 
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/console/parse.h>
+#include <pcl/common/transforms.h>
+
 #include "ground_filter.h"
 #include "plant_extraction.h"
 
@@ -40,9 +46,7 @@ using namespace er;
 #define NVDI -0.345f
 #define IR (0.361f * 255.f)
 #define MAX_LUMINOSITY (150.0f*3.0f)
-
-#define MAX_Z 3
-
+#define MIN_Z 0.2f
 
 void plants_filter::input_pcl(pcl_ptr cloud)
 {
@@ -63,8 +67,15 @@ bool plants_filter::process()
 		cloud_out->clear();
 	}
 
-	Eigen::RowVector3d cp;
+	Eigen::Transform<double, 3, Eigen::Affine>
+		ground_transform = grnd_filter->ground_transform;
 
+/*  In order to change keep orientation working we would have to render
+
+	pcl_ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZRGBA>());
+	pcl::transformPointCloud(*cloud_in, *transformed_cloud,
+		ground_transform.matrix());
+*/
 	for (auto& p : cloud_in->points) {
 		bool add_point = true;
 
@@ -75,23 +86,19 @@ bool plants_filter::process()
 			add_point = false;
 		} else {
 			float nvdi = float(p.a - p.r) / (p.a + p.r);
-
 			if (nvdi < NVDI) {
 				add_point = false;
 			}
 		}
 
-		if (add_point && p.z >= 0.01f)
+		if (add_point && p.z >= MIN_Z)
 			cloud_out->points.push_back(p);
 	}
 
 	if (view != nullptr) {
 		view->invalidate_cloud(cloud_out);
+
 		if (view->V.size() > 0) {
-
-			Eigen::Transform<double, 3, Eigen::Affine>
-				ground_transform = grnd_filter->ground_transform;
-
 			if (er::app_state::get().ground_alignment) {
 				for (int r = 0; r < view->V.rows(); r++) {
 					Eigen::Vector3d v = view->V.row(r);
